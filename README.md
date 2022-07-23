f92db3ec2923626f4efbed459e3a74554cc3c384
Sergey Pavlyuchenko <cergtli128@gmail.com>
2022-07-23 15:56:35 +0700

    Start_v.2

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Hi!
+<h1>The process of learning a new language Goland(GO)</h1>
--- main.go
+++ main.go
@@ -1,21 +1,28 @@
 // Где работаем!
 package main
- 
-// Импорт библеотек
-import ("fmt"
-		"net/http")
 
-func home_page(w http.ResponseWriter, r *http.Request)  {
+// Импорт библеотек
+import (
+	"fmt"
+	"net/http"
+)
+
+func home_page(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Ia rabotay ohyet")
 }
 
+func Oli_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Люлю ОЛЬКУ зайку")
+}
+
 // Отслежевание переходов по страницам
-func HandleRec()  {
+func HandleRec() {
 	http.HandleFunc("/", home_page)
+	http.HandleFunc("/Oli/", Oli_page)
 	http.ListenAndServe(":8080", nil)
 }
 
 // Основная функция где собираем дерево функций
-func main()  {
+func main() {
 	HandleRec()
 }
