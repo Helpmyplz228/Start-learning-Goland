42bb17a5d8e1cc359738c9c16c58d0afe9f09fc6
Sergey Pavlyuchenko <cergtli128@gmail.com>
2022-07-24 14:46:39 +0700

    Err on DB

--- Practice/main.go
+++ Practice/main.go
@@ -1,10 +1,61 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"net/http"
+	"text/template"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
+func index(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("template/index.html", "template/hiader.html", "template/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	t.ExecuteTemplate(w, "index", nil)
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("template/create.html", "template/hiader.html", "template/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	t.ExecuteTemplate(w, "create", nil)
+}
+
+func save_article(w http.ResponseWriter, r *http.Request) {
+	title := r.FormValue("title")
+	anons := r.FormValue("anons")
+	full_text := r.FormValue("full_text")
+
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/golang")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	//Усановка данных для базы
+	insert, err := db.Query(fmt.Sprintf("INSERT INTO `articles` (`title`, `anons`, `full_text`) VALUES('%s', '%s', '%s')", title, anons, full_text))
+	if err != nil {
+		panic(err)
+	}
+	defer insert.Close()
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+
+}
+
+func handelFunc() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	http.HandleFunc("/", index)
+	http.HandleFunc("/create/", create)
+	http.HandleFunc("/save_article/", save_article)
+	http.ListenAndServe(":8080", nil)
+}
+
 func main() {
-	fmt.Print("project start")
+	handelFunc()
 
 }
